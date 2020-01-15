72ca3cc37883e4ad693b7e66e1bd3f00d1d3ca51
Alex Vinicius Maximiano <55300851+naow-dev@users.noreply.github.com>
2020-01-15 19:27:38 -0300

    Frontend React - RadarDev

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-const express = require('express');
-//Importando a biblioteca
-const mongoose = require('mongoose');
-const routes = require('./routes');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const app = express();
-
-mongoose.connect('mongodb+srv://alex:alex1234@cluster0-kcrut.mongodb.net/test?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-});
-
-app.use(express.json()); // json tem que vir antes das Rotas. Para entender como vai ser lido
-app.use(routes);
+import App from './App';
 
 
-app.listen(3333);
+ReactDOM.render(<App />, document.getElementById('root'));
+
